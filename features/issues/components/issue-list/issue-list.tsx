6aa27077b1944856d589f7f1fd375e229a05893b
Anderson Luis Silva <andersonluis87@gmail.com>
2024-07-11 15:55:25 +0100

    Initialize react-query-tutorial-follow-along

---
+++ features/issues/components/issue-list/issue-list.tsx
@@ -0,0 +1,116 @@
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import { ProjectLanguage } from "@features/projects";
+import { color, space, textFont } from "@styles/theme";
+import { IssueRow } from "./issue-row";
+import { useState } from "react";
+
+const Container = styled.div`
+  background: white;
+  border: 1px solid ${color("gray", 200)};
+  box-sizing: border-box;
+  box-shadow: 0px 4px 8px -2px rgba(16, 24, 40, 0.1),
+    0px 2px 4px -2px rgba(16, 24, 40, 0.06);
+  border-radius: ${space(2)};
+  overflow: hidden;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+const HeaderRow = styled.tr`
+  border-bottom: 1px solid ${color("gray", 200)};
+`;
+
+const HeaderCell = styled.th`
+  padding: ${space(3, 6)};
+  text-align: left;
+  color: ${color("gray", 500)};
+  ${textFont("xs", "medium")};
+`;
+
+const PaginationContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${space(4, 6)};
+  border-top: 1px solid ${color("gray", 200)};
+`;
+
+const PaginationButton = styled.button`
+  height: 38px;
+  padding: ${space(0, 4)};
+  background: white;
+  border: 1px solid ${color("gray", 300)};
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  border-radius: 6px;
+
+  &:not(:first-of-type) {
+    margin-left: ${space(3)};
+  }
+`;
+
+const PageInfo = styled.div`
+  color: ${color("gray", 300)};
+  ${textFont("sm", "regular")}
+`;
+
+const PageNumber = styled.span`
+  ${textFont("sm", "medium")}
+`;
+
+const mockData = { items: [], meta: undefined };
+
+export function IssueList() {
+  const [page, setPage] = useState(1);
+
+  const { items, meta } = mockData;
+  const onClickResolve = (issueId: string) => null;
+
+  return (
+    <Container>
+      <Table>
+        <thead>
+          <HeaderRow>
+            <HeaderCell>Issue</HeaderCell>
+            <HeaderCell>Level</HeaderCell>
+            <HeaderCell>Events</HeaderCell>
+            <HeaderCell>Users</HeaderCell>
+          </HeaderRow>
+        </thead>
+        <tbody>
+          {(items || []).map((issue) => (
+            <IssueRow
+              key={issue.id}
+              issue={issue}
+              projectLanguage={ProjectLanguage.react}
+              resolveIssue={() => onClickResolve(issue.id)}
+            />
+          ))}
+        </tbody>
+      </Table>
+      <PaginationContainer>
+        <div>
+          <PaginationButton
+            onClick={() => setPage(page - 1)}
+            disabled={page === 1}
+          >
+            Previous
+          </PaginationButton>
+          <PaginationButton
+            onClick={() => setPage(page + 1)}
+            disabled={page === meta?.totalPages}
+          >
+            Next
+          </PaginationButton>
+        </div>
+        <PageInfo>
+          Page <PageNumber>{meta?.currentPage}</PageNumber> of{" "}
+          <PageNumber>{meta?.totalPages}</PageNumber>
+        </PageInfo>
+      </PaginationContainer>
+    </Container>
+  );
+}
